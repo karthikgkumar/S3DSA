72b3027ec00e5beabaa59ec517a80602645063b4
Karthik G Kumar <93706007+karthikgkumar@users.noreply.github.com>
2022-10-31 18:05:52 +1100

    cycle 3 added

---
+++ Stack3.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#define MAX 100
+int stack[MAX],top=-1;
+int auxStack[MAX],auxtop=-1;
+int isEmpty(int top)
+{
+    if(top==-1)
+        return 1;
+    else
+        return 0; 
+}
+void push(int data)
+{
+    if(top==MAX-1)
+        printf("\nStack Overflow");
+    else
+    {
+        stack[++top]=data;
+        if(isEmpty(auxtop))
+        {
+            auxStack[++auxtop]=data;
+        }  
+        else{
+            if(data<=auxStack[auxtop])
+            {
+                auxStack[++auxtop]=data;
+            }
+        }
+    }    
+}
+void pop()
+{
+    int del;
+    if(top==-1)
+    {
+        printf("\nStack empty.");
+    }
+    else{
+        del=stack[top--];
+        if(del==auxStack[auxtop])
+        {
+            auxtop--;
+        }
+        printf("\nDeleted : %d",del);
+    }
+}
+void displayStack()
+{
+    if (top==-1)
+        printf("\nStack empty");
+    else
+    {
+        printf("\n");
+        for (int i=top;i>=0;i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+void displayMin()
+{
+    if(auxtop==-1)
+        printf("\nStack Empty");
+    else
+        printf("\nMinimum Element : %d",auxStack[auxtop]);    
+}
+int main()
+{
+    int ch,element;
+    do
+    {
+        printf("\n\nChoose operation");
+        printf("\n1.Push");
+        printf("\n2.Pop");
+        printf("\n3.Display stack");
+        printf("\n4.Display min");
+        printf("\n5.Exit\n");
+        printf("\nEnter an operation\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:
+            printf("\nEnter element to insert\n");
+            scanf("%d",&element);
+            push(element);
+            break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                displayStack();
+                break;
+
+            case 4:
+                displayMin();
+                break;            
+        }
+    }while(ch<5);
+    return 0;
+}
---
+++ stack2.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#define MAX 100
+int stack[MAX],top1=-1,top2=MAX;
+void push(int stackno,int data)
+{
+     if (stackno==1)
+    {
+        if (top1==top2 - 1)
+        {
+            printf("\nStack 1 full");
+        }
+        else
+        {
+            stack[++top1]=data;
+        }
+    }
+    else if(stackno==2)
+    {
+        if (top2==top1+1)
+        {
+            printf("\nStack 2 full");
+        }
+        else
+        {
+            stack[--top2]=data;
+        }
+    }
+}
+int pop(int stackno)
+{
+    if (stackno==1)
+    {
+        if (top1==-1)
+        {
+            printf("\nStack 1 empty");
+            return -1;
+        }
+        return stack[top1--];
+    }
+    else if(stackno==2)
+    {
+        if (top2==MAX)
+        {
+            printf("\nStack 2 empty");
+            return -1;
+        }
+        return stack[top2++];
+    }
+}
+void display(int stackno)
+{
+    if (stackno==1)
+    {
+        if (top1==-1)
+        {
+            printf("\nStack 1 empty");
+            return;
+        }
+        printf("\nElements of stack 1\n");
+        for(int i=top1;i>=0;i--)
+            printf("%d ",stack[i]);
+    }
+    else if(stackno==2)
+    {
+        if (top2==MAX)
+        {
+            printf("\nStack 2 empty");
+            return;
+        }
+        printf("\nElements of stack 2\n");
+        for(int i=top2;i<=MAX-1;i++)
+            printf("%d ", stack[i]);
+    }
+}
+int main()
+{
+    int stackno, ch, element, popped;
+    printf("\nEnter stack number (1 or 2) ");
+    scanf("%d", &stackno);
+    do
+    {
+        printf("\n\nChoose operation");
+        printf("\n1.Push");
+        printf("\n2.Pop");
+        printf("\n3.Display");
+        printf("\n4.Change stack number");
+        printf("\n5.EXIT\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\nEnter element to insert ");
+            scanf("%d",&element);
+            push(stackno,element);
+            break;
+        case 2:
+            popped=pop(stackno);
+            printf("\nPopped element %d ",popped);
+            break;
+        case 3:
+            display(stackno);
+            break;
+        case 4:
+            printf("\nEnter stack number (1 or 2)");
+            scanf("%d", &stackno);
+            break;
+        }
+    } while(ch<5);
+    return 0;
+}
